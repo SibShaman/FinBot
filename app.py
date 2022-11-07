37bead0d1508c1287c74b2000e88def10b1c79b2
Shaman <infografika@inbox.ru>
2022-11-07 14:45:18 +0700

    add db

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 from flask import Flask
+# from flask_sqlalchemy import SQLAlchemy
+# from db.service_db import mysql_connect
 from handlers.main_handler import run_bot
 
 app = Flask(__name__)
 
+# Подключение к базе данных
+# app.config['SQLALCHEMY_DATABASE_URI'] = mysql_connect
+# db = SQLAlchemy(app)
 
-# @app.route('/')
-# def hello_world():  # put application's code here
-#     return 'Hello World!'
 
 run_bot()
 
